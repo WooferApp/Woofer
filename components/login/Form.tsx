d5f9768b1f28f0e1a7240cc1df592213de86655d
Virginie BAUMES <vbaumes@orchestra-premaman.com>
2022-04-04 15:26:12 +0200

    token ok rebase

--- components/login/Form.tsx
+++ components/login/Form.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { FlatList, StyleSheet } from "react-native";
 import { View, Text } from "../Themed";
 import React, { useReducer, useState } from "react";
@@ -71,9 +72,52 @@ export default function Form({ isRegistered }: FormProps) {
         .then(function (isValid) {
           console.log(isValid);
       });
-    }
-  };
+=======
+import {FlatList, StyleSheet} from "react-native";
+import {View, Text} from "../Themed";
+import React, {useReducer} from "react";
+import InputText from "../common/InputText";
+import StyledButton from "../common/StyledButton";
+import LoginReducer from "./reducers/LoginReducer";
+import {userLogin, setLoginError} from "./store/user.store";
+import axios from "axios";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../store/Store";
+import {API_URL} from "@env";
+import * as SecureStore from 'expo-secure-store';
 
+type FormProps = {
+    isRegistered: boolean;
+};
+
+export default function Form({isRegistered}: FormProps) {
+    const REGISTRATION = [
+        {name: "Name", type: "setName", textContentType: "name"},
+        {name: "Email", type: "setEmail", textContentType: "emailAddress"},
+        {name: "Password", type: "setPassword", textContentType: "password"},
+    ];
+    const LOGIN = [
+        {name: "Email", type: "setEmail", textContentType: "emailAddress"},
+        {name: "Password", type: "setPassword", textContentType: "password"},
+    ];
+    const {value: user, error} = useSelector((state: RootState) => state.user);
+    const [userToSign, setUserToSign] = useReducer(LoginReducer, {
+        name: "",
+        email: "",
+        password: "",
+    });
+    const dispatch = useDispatch();
+
+    async function setToken(token: any) {
+        return await SecureStore.setItemAsync('token', JSON.stringify(token));
+    }
+
+    async function getToken() {
+        return await SecureStore.getItemAsync('token');
+>>>>>>> 0bbfbbd (token okay)
+    }
+
+<<<<<<< HEAD
   return (
     <View  style={styles.container}>
       <FlatList
@@ -104,6 +148,61 @@ export default function Form({ isRegistered }: FormProps) {
       <StyledButton text="Submit" onPress={submit} />
     </View>
   );
+=======
+    const submit = async () => {
+        const {email, password, name} = userToSign;
+        if (isRegistered) {
+            try {
+                const response = await axios.post(API_URL, {
+                    username: email,
+                    password,
+                });
+                const {data} = response;
+                dispatch(userLogin({token: data.access_token, email}));
+                // navigation.navigate('ProfileScreen');
+                setToken({token: data.access_token, deadline: Date.now() + 6000000});
+                const token = await getToken();
+                if (typeof token === "string") {
+                    const tokenJSON = await JSON.parse(token);
+                    console.log(token);
+                    console.log('is valid', Date.now() < tokenJSON.deadline);
+                }
+
+
+                dispatch(userLogin({token: response.data.access_token, email}));
+            } catch (e) {
+                dispatch(setLoginError());
+            }
+        } else {
+            //post to route subscribe
+        }
+    };
+
+    return (
+        <View>
+            <FlatList
+                data={isRegistered ? LOGIN : REGISTRATION}
+                keyExtractor={(item, index) => item.name + index}
+                renderItem={({item}) => {
+                    return (
+                        <InputText
+                            term={item.name}
+                            onTermChange={(newTerm: React.SetStateAction<string>) =>
+                                setUserToSign({type: item.type, payload: newTerm})
+                            }
+                            onTermSubmit={() => {
+                            }}
+                            // @ts-ignore
+                            textContentType={item.textContentType}
+                        />
+                    );
+                }}
+            />
+            <StyledButton text="Submit" onPress={submit}/>
+            {error && <Text>{error}</Text>}
+        </View>
+    );
+>>>>>>> 0bbfbbd (token okay)
 }
 
 const styles = StyleSheet.create({
